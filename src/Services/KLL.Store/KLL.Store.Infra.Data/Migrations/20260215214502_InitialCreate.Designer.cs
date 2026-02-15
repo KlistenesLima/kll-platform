55ec1b27ff5f54ada970ad36084f297159c7f9ea
Klistenes de lima leite <KlistenesWar2@hotmail.com>
2026-02-15 18:45:34 -0300

    fix: corrigir migrations - gerar via EF CLI, fix filtro outbox, credenciais Postgres

--- src/Services/KLL.Store/KLL.Store.Infra.Data/Migrations/20260215214502_InitialCreate.Designer.cs
+++ src/Services/KLL.Store/KLL.Store.Infra.Data/Migrations/20260215214502_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using KLL.Store.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace KLL.Store.Infra.Data.Migrations
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260215214502_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
